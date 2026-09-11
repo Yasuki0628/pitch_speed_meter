dedbcea86c4454ba852e5b8bd9fd59d1caf1617f
Yasuki0628 <yasuki_maeshiro@kuniken.co.jp>
2026-09-11 10:50:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('fetch',e=>{});
+const CACHE_NAME="hope-speed-v1";
+const urlsToCache=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./favicon.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
